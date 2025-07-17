e77819616e99becca56ff869e1bdb3fafb23616d
AlfaAzkiya <alfa.azkiya@students.amikom.ac.id>
2025-07-17 19:18:31 +0700

    Menambahkan fitur dan memperbaiki bug

--- backend/users/signals.py
+++ backend/users/signals.py
@@ -2,6 +2,8 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from .models import User
 from lamaran.models import PelamarProfile
+from lamaran.models import PelamarProfile # <-- Impor PelamarProfile
+from django.db.models.signals import post_save, post_delete
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
@@ -12,3 +14,13 @@ def create_user_profile(sender, instance, created, **kwargs):
     # TAMBAHKAN KONDISI INI
     if created and instance.role == 'pelamar':
         PelamarProfile.objects.create(user=instance)
+
+@receiver(post_delete, sender=PelamarProfile)
+def hapus_file_pas_foto(sender, instance, **kwargs):
+    """
+    Sinyal ini akan menghapus file pas_foto secara fisik dari storage
+    saat objek PelamarProfile dihapus dari database.
+    """
+    # Pastikan instance memiliki pas_foto sebelum mencoba menghapusnya
+    if instance.pas_foto:
+        instance.pas_foto.delete(save=False)
